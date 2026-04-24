75466c6e610bc30cade4d2d6d72e4a522806f2a4
MufyDhaBeee <mufeedabeegum93@gmail.com>
2026-04-24 16:00:37 +0530

    tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -226,98 +226,250 @@
 //   }
 // }
 ///Example 4:
+// import 'package:flutter/material.dart';
+//
+// /// Flutter code sample for [AppBar].
+//
+// List<String> titles = <String>['Cloud', 'Beach', 'Sunny'];
+//
+// void main() => runApp(const AppBarApp());
+//
+// class AppBarApp extends StatelessWidget {
+//   const AppBarApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(colorSchemeSeed: const Color(0xff6750a4)),
+//       home: const AppBarExample(),
+//     );
+//   }
+// }
+//
+// class AppBarExample extends StatelessWidget {
+//   const AppBarExample({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final ColorScheme colorScheme = Theme.of(context).colorScheme;
+//     final Color oddItemColor = colorScheme.primary.withValues(alpha: 0.05);
+//     final Color evenItemColor = colorScheme.primary.withValues(alpha: 0.15);
+//     const int tabsCount = 3;
+//
+//     return DefaultTabController(
+//       initialIndex: 1,
+//       length: tabsCount,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('AppBar Sample'),
+//           // This check specifies which nested Scrollable's scroll notification
+//           // should be listened to.
+//           //
+//           // When `ThemeData.useMaterial3` is true and scroll view has
+//           // scrolled underneath the app bar, this updates the app bar
+//           // background color and elevation.
+//           //
+//           // This sets `notification.depth == 1` to listen to the scroll
+//           // notification from the nested `ListView.builder`.
+//           notificationPredicate: (ScrollNotification notification) {
+//             return notification.depth == 1;
+//           },
+//           // The elevation value of the app bar when scroll view has
+//           // scrolled underneath the app bar.
+//           scrolledUnderElevation: 4.0,
+//           shadowColor: Theme.of(context).shadowColor,
+//           bottom: TabBar(
+//             tabs: <Widget>[
+//               Tab(icon: const Icon(Icons.cloud_outlined), text: titles[0]),
+//               Tab(icon: const Icon(Icons.beach_access_sharp), text: titles[1]),
+//               Tab(icon: const Icon(Icons.brightness_5_sharp), text: titles[2]),
+//             ],
+//           ),
+//         ),
+//         body: TabBarView(
+//           children: <Widget>[
+//             ListView.builder(
+//               itemCount: 25,
+//               itemBuilder: (BuildContext context, int index) {
+//                 return ListTile(
+//                   tileColor: index.isOdd ? oddItemColor : evenItemColor,
+//                   title: Text('${titles[0]} $index'),
+//                 );
+//               },
+//             ),
+//             ListView.builder(
+//               itemCount: 25,
+//               itemBuilder: (BuildContext context, int index) {
+//                 return ListTile(
+//                   tileColor: index.isOdd ? oddItemColor : evenItemColor,
+//                   title: Text('${titles[1]} $index'),
+//                 );
+//               },
+//             ),
+//             ListView.builder(
+//               itemCount: 25,
+//               itemBuilder: (BuildContext context, int index) {
+//                 return ListTile(
+//                   tileColor: index.isOdd ? oddItemColor : evenItemColor,
+//                   title: Text('${titles[2]} $index'),
+//                 );
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+ ///TabBar------------------------------------------->>>
+// import 'package:flutter/material.dart';
+//
+// /// Flutter code sample for [TabBar].
+//
+// void main() => runApp(const TabBarApp());
+//
+// class TabBarApp extends StatelessWidget {
+//   const TabBarApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(home: TabBarExample());
+//   }
+// }
+//
+// class TabBarExample extends StatelessWidget {
+//   const TabBarExample({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       initialIndex: 1,
+//       length: 3,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('TabBar Sample'),
+//           bottom: const TabBar(
+//             tabs: <Widget>[
+//               Tab(icon: Icon(Icons.cloud_outlined)),
+//               Tab(icon: Icon(Icons.beach_access_sharp)),
+//               Tab(icon: Icon(Icons.brightness_5_sharp)),
+//             ],
+//           ),
+//         ),
+//         body: const TabBarView(
+//           children: <Widget>[
+//             Center(child: Text("It's cloudy here")),
+//             Center(child: Text("It's rainy here")),
+//             Center(child: Text("It's sunny here")),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+///Example 2:
+
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [AppBar].
+/// Flutter code sample for [TabBar].
 
-List<String> titles = <String>['Cloud', 'Beach', 'Sunny'];
+void main() => runApp(const TabBarApp());
 
-void main() => runApp(const AppBarApp());
-
-class AppBarApp extends StatelessWidget {
-  const AppBarApp({super.key});
+class TabBarApp extends StatelessWidget {
+  const TabBarApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(colorSchemeSeed: const Color(0xff6750a4)),
-      home: const AppBarExample(),
+    return const MaterialApp(home: TabBarExample());
+  }
+}
+
+class TabBarExample extends StatelessWidget {
+  const TabBarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Primary and secondary TabBar'),
+          bottom: const TabBar(
+            dividerColor: Colors.transparent,
+            tabs: <Widget>[
+              Tab(text: 'Flights', icon: Icon(Icons.flight)),
+              Tab(text: 'Trips', icon: Icon(Icons.luggage)),
+              Tab(text: 'Explore', icon: Icon(Icons.explore)),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            NestedTabBar('Flights'),
+            NestedTabBar('Trips'),
+            NestedTabBar('Explore'),
+          ],
+        ),
+      ),
     );
   }
 }
 
-class AppBarExample extends StatelessWidget {
-  const AppBarExample({super.key});
+class NestedTabBar extends StatefulWidget {
+  const NestedTabBar(this.outerTab, {super.key});
+
+  final String outerTab;
+
+  @override
+  State<NestedTabBar> createState() => _NestedTabBarState();
+}
+
+class _NestedTabBarState extends State<NestedTabBar>
+    with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final ColorScheme colorScheme = Theme.of(context).colorScheme;
-    final Color oddItemColor = colorScheme.primary.withValues(alpha: 0.05);
-    final Color evenItemColor = colorScheme.primary.withValues(alpha: 0.15);
-    const int tabsCount = 3;
-
-    return DefaultTabController(
-      initialIndex: 1,
-      length: tabsCount,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('AppBar Sample'),
-          // This check specifies which nested Scrollable's scroll notification
-          // should be listened to.
-          //
-          // When `ThemeData.useMaterial3` is true and scroll view has
-          // scrolled underneath the app bar, this updates the app bar
-          // background color and elevation.
-          //
-          // This sets `notification.depth == 1` to listen to the scroll
-          // notification from the nested `ListView.builder`.
-          notificationPredicate: (ScrollNotification notification) {
-            return notification.depth == 1;
-          },
-          // The elevation value of the app bar when scroll view has
-          // scrolled underneath the app bar.
-          scrolledUnderElevation: 4.0,
-          shadowColor: Theme.of(context).shadowColor,
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(icon: const Icon(Icons.cloud_outlined), text: titles[0]),
-              Tab(icon: const Icon(Icons.beach_access_sharp), text: titles[1]),
-              Tab(icon: const Icon(Icons.brightness_5_sharp), text: titles[2]),
+    return Column(
+      children: <Widget>[
+        TabBar.secondary(
+          controller: _tabController,
+          tabs: const <Widget>[
+            Tab(text: 'Overview'),
+            Tab(text: 'Specifications'),
+          ],
+        ),
+        Expanded(
+          child: TabBarView(
+            controller: _tabController,
+            children: <Widget>[
+              Card(
+                margin: const EdgeInsets.all(16.0),
+                child: Center(child: Text('${widget.outerTab}: Overview tab')),
+              ),
+              Card(
+                margin: const EdgeInsets.all(16.0),
+                child: Center(
+                  child: Text('${widget.outerTab}: Specifications tab'),
+                ),
+              ),
             ],
           ),
         ),
-        body: TabBarView(
-          children: <Widget>[
-            ListView.builder(
-              itemCount: 25,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  tileColor: index.isOdd ? oddItemColor : evenItemColor,
-                  title: Text('${titles[0]} $index'),
-                );
-              },
-            ),
-            ListView.builder(
-              itemCount: 25,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  tileColor: index.isOdd ? oddItemColor : evenItemColor,
-                  title: Text('${titles[1]} $index'),
-                );
-              },
-            ),
-            ListView.builder(
-              itemCount: 25,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  tileColor: index.isOdd ? oddItemColor : evenItemColor,
-                  title: Text('${titles[2]} $index'),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
+      ],
     );
   }
 }
